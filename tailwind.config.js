46fbb5f700d25e80ce8b4a8b71244ad78e90718f
Junaid <sj.shaikhjunaid@gmail.com>
2022-04-30 01:06:05 +0530

    [style] add color for background

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,8 @@ module.exports = {
     backgroundColor: {
       grey: "hsl(210, 23%, 95%)",
       hoverClr: "#28292c11",
+      primary: "hsl(245, 53%, 53%)",
+      primaryDark: "hsl(245, 53%, 33%)",
     },
     minHeight: {
       60: "60vh",
