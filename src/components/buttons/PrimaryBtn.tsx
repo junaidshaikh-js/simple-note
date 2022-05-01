15a645a6b9990c97a7042f2ec310f4c61d292634
Junaid <sj.shaikhjunaid@gmail.com>
2022-05-02 02:20:55 +0530

    update button component for type attribute

--- src/components/buttons/PrimaryBtn.tsx
+++ src/components/buttons/PrimaryBtn.tsx
@@ -1,10 +1,22 @@
 type PrimaryBtnProps = {
   children: string;
+  type?: "button" | "submit" | "reset";
+  cnames?: string;
 };
 
-export const PrimaryBtn = ({ children }: PrimaryBtnProps) => {
+export const PrimaryBtn = ({
+  children,
+  type = "button",
+  cnames,
+}: PrimaryBtnProps) => {
   return (
-    <button className="py-2 px-2.5 bg-primary text-white rounded hover:bg-primaryDark">
+    <button
+      className={
+        "py-2 px-2.5 bg-primary text-white rounded hover:bg-primaryDark " +
+        cnames
+      }
+      type={type}
+    >
       {children}
     </button>
   );
