fce74a17a17e59fc5d3b25f579ab9db00f934eed
Junaid <sj.shaikhjunaid@gmail.com>
2022-05-02 02:22:39 +0530

    header component should be resused on auth pages

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,23 +1,30 @@
 import { FaBars } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 type HeaderProps = {
-  setIsSideMenuOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  setIsSideMenuOpen?: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
 export const Header = ({ setIsSideMenuOpen }: HeaderProps) => {
   return (
-    <div className="bg-white sticky top-0">
+    <div className="bg-white sticky top-0 border-b border-black">
       <header className="bg-white p-3 flex items-center	md:ml-10">
         <div
           className="mr-5 cursor-pointer md:hidden"
-          onClick={() => setIsSideMenuOpen((s) => !s)}
+          onClick={() => {
+            if (setIsSideMenuOpen) {
+              setIsSideMenuOpen((s) => !s);
+            }
+          }}
         >
-          <FaBars fontSize="1.5rem" />
+          {setIsSideMenuOpen && <FaBars fontSize="1.5rem" />}
         </div>
         <div>
-          <span className="font-bold text-3xl">
-            Simple<span className="text-primary">Note</span>
-          </span>
+          <Link to="/">
+            <span className="font-bold text-3xl">
+              Simple<span className="text-primary">Note</span>
+            </span>
+          </Link>
         </div>
       </header>
     </div>
