bb174ca292c50845b53212889a0679976db9fdac
Junaid <sj.shaikhjunaid@gmail.com>
2022-05-07 17:00:53 +0530

    add toaster component and display navigation on user state

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
+import { Toaster } from "react-hot-toast";
+
 import { Navigation } from "./components";
 import { Router } from "./Router";
+import { useAuth } from "./context";
 
 function App() {
-  let token = false;
+  const authData = useAuth();
 
   return (
     <div>
-      {token && <Navigation />}
+      <Toaster
+        position="bottom-center"
+        toastOptions={{
+          style: {
+            background: "black",
+            color: "#fff",
+          },
+        }}
+      />
+
+      {authData.uid && <Navigation />}
       <Router />
     </div>
   );
