ca0ffbbe6a9f0e00da86a948fd3c8e97523e50e6
Junaid <sj.shaikhjunaid@gmail.com>
2022-05-24 13:07:57 +0530

    [fix]  merge conflict

--- src/pages/Label/Label.tsx
+++ src/pages/Label/Label.tsx
@@ -26,9 +26,7 @@ export const Label = () => {
       ) : (
         labelNotes.map((note) => {
           return (
-            <article
-              className={`p-2 my-5 mx-5 md:mx-auto border-2 border-black rounded-lg max-w-3xl mx-auto whitespace-pre-wrap break-words bg-${note.bgColor}`}
-            >
+            <article className="p-2 my-5 mx-5 md:mx-auto border-2 border-black rounded-lg max-w-3xl mx-auto whitespace-pre-wrap break-words">
               <div>
                 <h2 className="text-xl font-semibold text-xl mb-3">
                   {note.title}
