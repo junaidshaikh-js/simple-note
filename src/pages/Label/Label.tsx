1230572aedfa7d83cbda2654eca8437b14e050b4
Junaid <sj.shaikhjunaid@gmail.com>
2022-05-24 13:00:03 +0530

    trash page

--- src/pages/Label/Label.tsx
+++ src/pages/Label/Label.tsx
@@ -18,13 +18,36 @@ export const Label = () => {
   return (
     <div className="md:ml-72">
       {!labelNotes.length ? (
-        <section className="flex items-center justify-center flex-col min-h-70 text-gray-500 text-xl">
+        <main className="flex items-center justify-center flex-col min-h-70 text-gray-500 text-xl">
           <MdLabelOutline className="text-6xl" />
           <p>No notes with label yet.</p>
-        </section>
+        </main>
       ) : (
         labelNotes.map((note) => {
+<<<<<<< Updated upstream
           return <p>{note.noteText}</p>;
+=======
+          return (
+            <main>
+              <article
+                className={`p-2 my-5 mx-5 md:mx-auto border-2 border-black rounded-lg max-w-3xl mx-auto whitespace-pre-wrap break-words bg-${note.bgColor}`}
+              >
+                <div>
+                  <h2 className="text-xl font-semibold text-xl mb-3">
+                    {note.title}
+                  </h2>
+                  <p>{note.noteText}</p>
+                </div>
+
+                <div className="my-2 flex">
+                  {note.labels.map((label) => (
+                    <LabelTag currentLabel={label} noteId={note.id} />
+                  ))}
+                </div>
+              </article>
+            </main>
+          );
+>>>>>>> Stashed changes
         })
       )}
     </div>
