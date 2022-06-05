e43e7cdc5359d0d44c9bf4a63c65cee60714bf7e
Junaid <sj.shaikhjunaid@gmail.com>
2022-06-06 01:40:53 +0530

    center notes on label page

--- src/pages/Label/Label.tsx
+++ src/pages/Label/Label.tsx
@@ -23,7 +23,7 @@ export const Label = () => {
         labelNotes.map((note) => {
           return (
             <article
-              className={`p-2 my-5 mx-5 border-2 border-black rounded-lg max-w-3xl mx-auto whitespace-pre-wrap break-words bg-${note.bgColor}`}
+              className={`p-2 my-5 mx-5 border-2 border-black rounded-lg max-w-3xl mx-auto whitespace-pre-wrap break-words bg-${note.bgColor} md:mx-auto`}
             >
               <div>
                 <h2 className="text-xl font-semibold text-xl mb-3">
